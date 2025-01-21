0e6a283514aa19cf2bbd2f96e9dd938466ccbb1a
Mateus <mateusbarbozamartins@gmail.com>
2025-01-21 14:30:32 -0300

    Criando cards de projetos feitos

--- src/components/Body/Projetos/ListCards.tsx
+++ src/components/Body/Projetos/ListCards.tsx
@@ -2,10 +2,13 @@ import Card from "./Card";
 
 export default function ListCards() {
     return ( 
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center items-center">
+            <h1 className=" text-2xl font-bold uppercase mt-20">
+                Projetos
+            </h1>
             <div className={`
                 flex justify-center w-[80%] 
-                mt-20 mb-20 gap-5 p-10
+                mt-10 mb-20 gap-5 p-10
                 overflow-x-auto scroll-hidden
             `}>
                 <Card />
