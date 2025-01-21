1dc9e6d431b7392276b583c2927699b1fc4c1f37
Mateus <mateusbarbozamartins@gmail.com>
2025-01-21 16:07:22 -0300

    Retirando icon de components

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -32,14 +32,14 @@ export const IconLink = ({ className = "" }: { className?: string }) => (
 )
 
 export const IconArrowLeft = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
 </svg>
 
 )
 
 export const IconArrowRight = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
 </svg>
 
